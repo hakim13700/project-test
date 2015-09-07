0d89542417191325c9c68a0af0f24e0c95861b7a
stagiaire <stagiaire@Stagiaire06>
2015-09-07 09:59:07 +0200

    firt commit

---
+++ project-test/src/fr/treeptik/test/test.java
@@ -0,0 +1,15 @@
+package fr.treeptik.test;
+
+public class test {
+
+	private String test;
+
+	public String getTest() {
+		return test;
+	}
+
+	public void setTest(String test) {
+		this.test = test;
+	}
+	
+}
